fc96cd8b8cf07bd8a14e6bb1fd810f5ade71719b
bempache <vicente.guarda@usach.cl>
2025-05-28 16:41:36 -0400

    Add files via upload

---
+++ Nuevo documento de texto.py
@@ -0,0 +1,12 @@
+from dearpygui.core import *
+from dearpygui.simple import *
+####
+# CREAR VENTANA set_main_window_size(ANCHOxALTURA)
+# AJUSTAR TAMAÑO DE TEXTOS set_global_font_scale(NUMERO 6.66)
+# COLOR WEA DE ARRIBA set_theme(BUSACR THEMES DISPONIBLES)
+# CREA LA VENTANA CON NOMBRE with window("NOMBRE DE VENTANA", width= NUM, height= NUM)
+# CORRE EL PROGRAMA start_dearpygui()
+#
+# EJ
+#
+
