a534bccf9d1fa43785cf938a406f29ef7a243463
bempache <vicente.guarda@usach.cl>
2025-06-18 16:43:23 -0400

    p.py

--- hola/p.py
+++ hola/p.py
@@ -1,5 +1,8 @@
 import openpyxl as ox
+import tkinter as tk
+from matplotlib import *
 
+###obt datos###
 datos = []
 
 hoja = ox.load_workbook("nombre.xlsx")
@@ -14,7 +17,27 @@ for fila in range(1, hoja_data.max_row):
     for columna in hoja_data.iter_cols(0, hoja_data.max_column):
         _fila.append(columna[fila].value)
     datos.append(_fila)
-
 print(datos)
+seccion = []
+alumnos = []
+notas = []
 
+
+
+i = 0
+while i < len(datos):
+    seccion.append(datos[i][0])
+    i+=1
+print(seccion)
+i = 0
+while i < len(datos):
+    alumnos.append(datos[i][1])
+    i+=1
+print(alumnos)
+i = 0
+while i < len(datos):
+    notas.append(datos[i][2])
+    i+=1
+print(notas)
+###obt datos###
 
